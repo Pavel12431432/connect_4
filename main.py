f22a60dd9b14d9e06b3c8b90c9f353f1e0fc8fbe
Pavel <49321976+Pavel12431432@users.noreply.github.com>
2020-10-19 17:26:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,116 +1,166 @@
-from os import system
+import pygame
 
+# define constants
 how_many_to_connect = 4
-background_color = 40
-ROW_COUNT = 1 + 6
 COL_COUNT = 7
-player_1_color = 31
-player_2_color = 32
-size = (3, 5)
+ROW_COUNT = 6
+TILE_SIZE = 128
+MAX_FPS = 144
+FALLING_SPEED = 22
+WIDTH, HEIGHT = COL_COUNT * TILE_SIZE, ROW_COUNT * TILE_SIZE
 
-board = [[0] * COL_COUNT for i in range(ROW_COUNT)]
+# pygame init
+pygame.mixer.pre_init(44100, -16, 1, 4096)
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Connect ' + str(how_many_to_connect))
+pygame.display.set_icon(pygame.transform.scale(pygame.image.load('icon.png'), (32, 32)))
+font = pygame.font.SysFont('impact', 120)
+clock = pygame.time.Clock()
+
+# import sounds
+hit_sound = pygame.mixer.Sound('audio/hit.wav')
+win_sound = pygame.mixer.Sound('audio/win.wav')
+turn = 1
+
+# import images
+imgs = [
+	pygame.transform.scale(pygame.image.load('sprites/empty.png').convert_alpha(), (TILE_SIZE, TILE_SIZE)),
+	pygame.transform.scale(pygame.image.load('sprites/red.png').convert_alpha(), (TILE_SIZE, TILE_SIZE)),
+	pygame.transform.scale(pygame.image.load('sprites/yellow.png').convert_alpha(), (TILE_SIZE, TILE_SIZE)),
+	pygame.transform.scale(pygame.image.load('sprites/selected.png').convert_alpha(), (TILE_SIZE, TILE_SIZE))
+]
 
 
-def printBoard():
-	system('cls')
-	for i in range(1, COL_COUNT + 1):
-		print(i, end=' ' * (5 + 1 - len(str(i))))
-	print('')
-	for y in range(ROW_COUNT):
-		for r in range(size[0]):
-			for x in range(COL_COUNT):
-				if board[y][x] == 1:
-					print("\033[1;" + str(player_1_color) + ";" + str(background_color) + "m", end='')
-				elif board[y][x] == 2:
-					print("\033[1;" + str(player_2_color) + ";" + str(background_color) + "m", end='')
-				else:
-					print("\033[1;34;40m", end='')
-				print('▓' * size[1], end='\033[1;37;40m ')
-			print()
-		print()
+class Piece:
+	def __init__(self, img_index, goal, moving):
+		self.img = img_index
+		self.pos = 0
+		self.goal = goal
+		self.moving = moving
+
+	def update(self):
+		# update position
+		self.pos = min(self.pos + FALLING_SPEED * self.moving, self.goal)
+		# set moving to false if goal is met
+		self.moving = not self.pos == self.goal
 
 
+# place a piece on certain column
+def place_piece(col_index, img_index):
+	try:
+		column = board[col_index]
+		# find lowest open spot
+		index = [len(column) - i - 1 for i in range(len(column[::-1])) if not column[::-1][i].img][0]
+		# place piece
+		board[col_index][index] = Piece(img_index, index * TILE_SIZE - 1, True)
+		# play sound
+		pygame.mixer.Sound.play(hit_sound)
+		# return 1 if piece was placed
+		return 1
+	except (IndexError, TypeError):
+		# return 0 if unable to place piece
+		return 0
+
+
+# draw board
+def draw(mouse_pos):
+	screen.fill((37, 37, 37))
+
+	# draw each piece
+	for col in range(len(board)):
+		for piece in range(len(board[col])):
+			p = board[col][piece]
+			if p.img:
+				# draw current piece
+				screen.blit(imgs[p.img], (col * TILE_SIZE, p.pos))
+				# update the current piece's position
+				p.update()
+
+	# draw grid
+	for col in range(len(board)):
+		for piece in range(len(board[col])):
+			screen.blit(imgs[0], (col * TILE_SIZE, piece * TILE_SIZE))
+
+	# draw selected space
+	screen.blit(imgs[3], (mouse_pos[0] // TILE_SIZE * TILE_SIZE, mouse_pos[1] // TILE_SIZE * TILE_SIZE))
+
+	pygame.display.update()
+
+
+# check if game has ended
 def winning_move(piece):
+	# Check horizontal locations for win
 	for c in range(COL_COUNT - (how_many_to_connect - 1)):
 		for r in range(ROW_COUNT):
-			win = True
-			for i in range(how_many_to_connect):
-				if board[r][c + i] != piece:
-					win = False
-					break
-			if win:
+			if min([board[c + i][r].img == piece for i in range(how_many_to_connect)]):
 				return True
 
+	# Check vertical locations for win
 	for c in range(COL_COUNT):
 		for r in range(ROW_COUNT - (how_many_to_connect - 1)):
-			win = True
-			for i in range(how_many_to_connect):
-				if board[r + i][c] != piece:
-					win = False
-					break
-			if win:
+			if min([board[c][r + i].img == piece for i in range(how_many_to_connect)]):
 				return True
 
+	# Check \ diagonals
 	for c in range(COL_COUNT - (how_many_to_connect - 1)):
 		for r in range(ROW_COUNT - (how_many_to_connect - 1)):
-			win = True
-			for i in range(how_many_to_connect):
-				if board[r + i][c + i] != piece:
-					win = False
-					break
-			if win:
+			if min([board[c + i][r + i].img == piece for i in range(how_many_to_connect)]):
 				return True
 
+	# check / diagonals
 	for c in range(COL_COUNT - (how_many_to_connect - 1)):
 		for r in range((how_many_to_connect - 1), ROW_COUNT):
-			win = True
-			for i in range(how_many_to_connect):
-				if board[r - i][c + i] != piece:
-					win = False
-					break
-			if win:
+			if min([board[c + i][r - i].img == piece for i in range(how_many_to_connect)]):
 				return True
 
 
+# handle input
 def inp():
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			quit()
+		elif event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+			return pygame.mouse.get_pos()[0] // TILE_SIZE
+
+
+def end_game():
+	# finish drawing falling pieces
 	while True:
-		try:
-			a = int(input())
-			if 0 < a <= COL_COUNT:
-				return a
-		except:
-			pass
+		# check if any piece is moving
+		if max([max([board[col][piece].moving for piece in range(len(board[col]))]) for col in range(len(board))]):
+			draw(pygame.mouse.get_pos())
+		else:
+			draw(pygame.mouse.get_pos())
+			break
+		inp()
+		clock.tick(MAX_FPS)
+
+	# show victory text
+	text = font.render('Player {} has won!'.format(turn), True, (255, 255, 255))
+	text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 3))
+	screen.blit(text, text_rect)
+	pygame.display.update()
+
+	# play win sound
+	pygame.mixer.Sound.play(win_sound)
+
+	while True:
+		inp()
 
 
-def getRow(col):
-	for y in range(ROW_COUNT):
-		if board[y][col]:
-			return y - 1
-		if y == ROW_COUNT - 1:
-			return ROW_COUNT - 1
+# init board
+board = [[Piece(0, piece * TILE_SIZE, False) for col in range(ROW_COUNT)] for piece in range(COL_COUNT)]
 
-
-def drop(col, turn):
-	row = getRow(col)
-	if row:
-		board[row][col] = turn
-
-
-def loop():
-	turn = 0
-	gameOver = False
-	while not gameOver:
-		printBoard()
-		print("Player {}'s turn: ".format(turn + 1))
-		turn = not turn
-		col = inp() - 1
-		drop(col, turn + 1)
-		if winning_move(turn + 1):
-			printBoard()
-			print("Player {} has won".format(turn + 1))
-			return
-
-
-if __name__ == '__main__':
-	loop()
-	input('Press enter to continue...')
+# main loop
+while True:
+	# check if a piece has been placed
+	if (i := inp()) != None and place_piece(i, turn):
+		# check if a player has won
+		if winning_move(turn):
+			end_game()
+		# change turn
+		turn = 2 if turn == 1 else 1
+	# draw board
+	draw(pygame.mouse.get_pos())
+	clock.tick(MAX_FPS)
